42dfd41e74c2caf9a7c9d76de001bf3141c150ca
Jankin_Chen <2390919165@qq.com>
2026-07-20 15:46:31 +0800

    chore: ignore local superpowers files

--- .superpowers/brainstorm/1676-1784268892/start-server.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set BRAINSTORM_PORT=62893
-set BRAINSTORM_HOST=127.0.0.1
-set BRAINSTORM_URL_HOST=localhost
-set BRAINSTORM_DIR=D:\HuaweiMoveData\Users\23909\Desktop\cpp-learn-agent\.superpowers\brainstorm\1676-1784268892
-"C:\Program Files\nodejs\node.exe" "D:\.codex\plugins\cache\openai-api-curated\superpowers\11c74d6b\skills\brainstorming\scripts\server.cjs"
